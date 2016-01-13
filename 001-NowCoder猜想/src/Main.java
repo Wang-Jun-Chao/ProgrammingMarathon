e79626d24eada0991519653e67b9288ffeb6babe
user <oracleer@foxmail.com>
2016-01-13 13:55:15 +0800

    Initial commit

---
+++ 001-NowCoder猜想/src/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Author: 王俊超
+ * Date: 2016-01-13 13:54
+ * CSDN: http://blog.csdn.net/derrantcm
+ * Github: https://github.com/Wang-Jun-Chao
+ * All Rights Reserved !!!
+ */
+public class Main {
+}

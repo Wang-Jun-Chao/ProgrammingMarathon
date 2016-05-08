7c3b01e519e7ea46df3702ccdd62176c55249218
Wang-Jun-Chao <shining-glory@qq.com>
2016-05-08 13:41:45 +0800

    commit

--- 005-猴子分桃/src/Main.java
+++ 005-猴子分桃/src/Main.java
@@ -9,8 +9,8 @@ import java.util.Scanner;
  */
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-//        Scanner scanner = new Scanner(Main.class.getClassLoader().getResourceAsStream("data.txt"));
+//        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(Main.class.getClassLoader().getResourceAsStream("data.txt"));
         while (scanner.hasNext()) {
             int n = scanner.nextInt();
             if (n == 0) {
--- 006-统计一/src/Main.java
+++ 006-统计一/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Author: 王俊超
  * Time: 2016-05-07 12:14
@@ -7,9 +9,24 @@
  */
 public class Main {
     public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(Main.class.getClassLoader().getResourceAsStream("data.txt"));
+        while (scanner.hasNext()) {
+            long n = scanner.nextLong();
+            long r = countOne(n);
+            System.out.println(r);
+        }
 
+        scanner.close();
     }
 
+    /**
+     * 统计1出现的次数
+     * 见word解法一
+     *
+     * @param n 最大的数字
+     * @return 1出现的次数
+     */
     private static long countOne3(long n) {
         long count = 0;
         for (int i = 0, j; i <= n; i++) {
@@ -22,5 +39,45 @@ public class Main {
             }
         }
         return count;
+    }
+
+    /**
+     * 统计1出现的次数
+     * 见word解法一
+     *
+     * @param n 最大的数字
+     * @return 1出现的次数
+     */
+    private static long countOne(long n) {
+        if (n <= 0) {
+            return 0;
+        } else if (n < 10) {
+            return 1;
+        } else {
+            long count = 0;
+
+            // 最高位的数字
+            long highest = n;
+            // 数据位数-1
+            int bit = 0;
+            //代表最高位的权重，即最高位一个1代表的大小
+            int power10 = 1;
+            while (highest >= 10) {
+                highest /= 10;
+                bit++;
+                power10 *= 10;
+            }
+
+            if (highest == 1) {
+                count = countOne(power10 - 1)
+                        + countOne(n - power10)
+                        + n - power10 + 1;
+            } else {
+                count = highest * countOne(power10 - 1)
+                        + countOne(n - highest * power10)
+                        + power10;
+            }
+            return count;
+        }
     }
 }

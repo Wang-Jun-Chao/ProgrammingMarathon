ec2fb73813b771af0832a3274934db8ab966fe01
王俊超 <shining-glory@qq.com>
2016-05-15 22:21:38 +0800

    commit

---
+++ 047-发工资/src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * Author: 王俊超
+ * Time: 2016-05-15 22:20
+ * CSDN: http://blog.csdn.net/derrantcm
+ * Github: https://github.com/Wang-Jun-Chao
+ * Declaration: All Rights Reserved !!!
+ */
+public class Main {
+    // 对每个员工使用贪心算法
+}

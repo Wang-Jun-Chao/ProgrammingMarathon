58a96d46ea8185ee9976fed502ac0fb23d4d1570
admin <shiinnny-glory@foxmail.com>
2016-05-15 09:12:31 +0800

    commit

--- 039-寻找舞伴/src/Main.java
+++ 039-寻找舞伴/src/Main.java
@@ -34,23 +34,40 @@ public class Main {
         scanner.close();
     }
 
+    /**
+     * 求舞伴的对数
+     *
+     * @param boy  男生身高数组
+     * @param girl 女生身高数组
+     * @return 舞伴的对数
+     */
     private static int match(int[] boy, int[] girl) {
 
+        // 排序
         Arrays.sort(boy);
         Arrays.sort(girl);
 
+        // 记录舞伴的对数
         int rst = 0;
+        // 当前等待匹配的男生数组下标
         int i = 0;
+        // 当前等待匹配的女生数组下标
         int j = 0;
 
+        // 对男生女生进行处理，直到男生或者女生都处理完
         while (i < boy.length && j < girl.length) {
+            // 身高相等，对数加一，处理下一对
             if (boy[i] == girl[j]) {
                 rst++;
                 i++;
                 j++;
-            } else if (boy[i] - girl[j] > 0) {
+            }
+            // 女生矮，选择下一个女生
+            else if (boy[i] - girl[j] > 0) {
                 j++;
-            } else {
+            }
+            // 男生矮，选择下一个男生
+            else {
                 i++;
             }
         }

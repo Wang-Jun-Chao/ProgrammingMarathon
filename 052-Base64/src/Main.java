ff2081cfc9927dea7f62242fb49fe27336bcb85e
admin <shiinnny-glory@foxmail.com>
2016-05-16 19:45:12 +0800

    commit

--- 052-Base64/src/Main.java
+++ 052-Base64/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Author: 王俊超
  * Time: 2016-05-16 19:14
@@ -6,4 +8,40 @@
  * Declaration: All Rights Reserved !!!
  */
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+//        Scanner scanner = new Scanner(Main.class.getClassLoader().getResourceAsStream("data.txt"));
+        while (scanner.hasNextLine()) {
+            String s = scanner.nextLine();
+
+            System.out.println(split(s));
+        }
+
+        scanner.close();
+    }
+
+    /**
+     * 分割字符串s，每76个字符插入一个换行符
+     *
+     * @param s 字符串
+     * @return 分割后的字符串
+     */
+    private static String split(String s) {
+        int i = 0;
+        int n = 76;
+
+        int len = s.length();
+        StringBuilder b = new StringBuilder(s.length());
+        while (i < len) {
+            if (i + n <= len) {
+                b.append(s.substring(i, i + n)).append('\n');
+            } else {
+                b.append(s.substring(i)).append('\n');
+            }
+
+            i += n;
+        }
+
+        return b.toString();
+    }
 }

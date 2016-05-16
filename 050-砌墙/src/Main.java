2e779b5764bc52962d1f21d822dedd85a842f5a6
admin <shiinnny-glory@foxmail.com>
2016-05-16 17:10:20 +0800

    commit

--- 050-砌墙/src/Main.java
+++ 050-砌墙/src/Main.java
@@ -9,10 +9,28 @@ import java.util.Scanner;
  */
 public class Main {
     public static void main(String[] args) {
-//        Scanner scanner = new Scanner(System.in);
-        Scanner scanner = new Scanner(Main.class.getClassLoader().getResourceAsStream("data.txt"));
+        Scanner scanner = new Scanner(System.in);
+//        Scanner scanner = new Scanner(Main.class.getClassLoader().getResourceAsStream("data.txt"));
         while (scanner.hasNext()) {
-            int line = scanner.nextInt();
+            int n = scanner.nextInt();
+
+            int avg = 0;
+            int[] pillar = new int[n];
+
+            for (int i = 0; i < n; i++) {
+                pillar[i] = scanner.nextInt();
+                avg += pillar[i];
+            }
+
+            avg /= n;
+            int move = 0;
+            for (int i : pillar) {
+                if (i > avg) {
+                    move += i - avg;
+                }
+            }
+
+            System.out.println(move);
         }
 
         scanner.close();

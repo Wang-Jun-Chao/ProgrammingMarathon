a9b6bf422e994f7ae53f3d6c101c598b6a9b8457
admin <shiinnny-glory@foxmail.com>
2016-05-16 14:37:58 +0800

    commit

--- 049-最佳投资/src/Main.java
+++ 049-最佳投资/src/Main.java
@@ -10,7 +10,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-//        Scanner scanner = new Scanner(Main.class.getClassLoader().getResourceAsStream("data.txt"));
+//        Scanner scanner = new Scanner(Main.class.getClassLoader().getResourceAsStream("data2.txt"));
         while (scanner.hasNext()) {
             double[] stock = new double[9];
             for (int i = 0; i < stock.length; i++) {
@@ -24,12 +24,39 @@ public class Main {
     }
 
     /**
-     * @param stock
-     * @return
+     * 最佳投资方案，求股票买入卖出可以获得的最大收益
+     *
+     * @param stock 股票价格格数据，下标小的表示较早的时间
+     * @return 最大收益，0表示不投资
      */
     private static double bestInvest(double[] stock) {
 
+        // 记录最大的差价，卖出时间不早于买入时间
+        double maxDiff = 0;
+        // 股票买入时间
+        int in = 0;
+        // 股票卖出时间
+        int out = 0;
 
-        return 0;
+        // 从第二个数据开始处理
+        for (int i = 1, j = stock.length; i < j; i++) {
+
+            // i时刻的股票价格比记录到的最小的价格低，在此处买进，有可能赚到钱
+            if (stock[i] < stock[in]) {
+                in = i;
+                out = i;
+                continue;
+            }
+
+            // i时刻的股票价格比记录到的最高的价格不高，并且比之前记录到的最大差值还大
+            // 在此处卖出，能赚到更多的钱
+            if (stock[i] > stock[out] && maxDiff < stock[i] - stock[in]) {
+                out = i;
+                maxDiff = stock[out] - stock[in];
+            }
+        }
+
+
+        return maxDiff;
     }
 }

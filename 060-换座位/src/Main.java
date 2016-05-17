e2509492fe7c42a573fd4f347c6fbac8dc155e3c
admin <shiinnny-glory@foxmail.com>
2016-05-17 18:32:55 +0800

    commit

---
+++ 060-换座位/src/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Author: 王俊超
+ * Time: 2016-05-17 18:31
+ * CSDN: http://blog.csdn.net/derrantcm
+ * Github: https://github.com/Wang-Jun-Chao
+ * Declaration: All Rights Reserved !!!
+ */
+public class Main {
+}

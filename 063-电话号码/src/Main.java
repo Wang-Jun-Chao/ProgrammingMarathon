4e52e18691251f95ef4d822bc6d63f6ce3ee0d36
admin <shiinnny-glory@foxmail.com>
2016-05-17 20:49:49 +0800

    commit

--- 063-电话号码/src/Main.java
+++ 063-电话号码/src/Main.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
 
 /**
  * Author: 王俊超
@@ -8,13 +10,55 @@ import java.util.Scanner;
  * Declaration: All Rights Reserved !!!
  */
 public class Main {
+    // 大写字母转换成对应数字的掩码表
+    private static char[] MASK = {
+            '2', '2', '2', '3', '3', '3', '4', '4', '4', '5',
+            '5', '5', '6', '6', '6', '7', '7', '7', '7', '8',
+            '8', '8', '9', '9', '9', '9'};
+
     public static void main(String[] args) {
-//        Scanner scanner = new Scanner(System.in);
-        Scanner scanner = new Scanner(Main.class.getClassLoader().getResourceAsStream("data.txt"));
+        Scanner scanner = new Scanner(System.in);
+//        Scanner scanner = new Scanner(Main.class.getClassLoader().getResourceAsStream("data.txt"));
         while (scanner.hasNext()) {
-            int line = scanner.nextInt();
+            int n = scanner.nextInt();
+            String[] tel = new String[n];
+            for (int i = 0; i < n; i++) {
+                tel[i] = scanner.next();
+            }
+
+            Set<String> ret = convert(tel);
+            for (String s : ret) {
+                System.out.println(s);
+            }
+            System.out.println();
         }
 
         scanner.close();
+    }
+
+    private static Set<String> convert(String[] tel) {
+        Set<String> rst = new TreeSet<>();
+        char[] arr = new char[8];
+        int count;
+        for (String s : tel) {
+            count = 0;
+            for (int i = 0, j = s.length(); i < j; i++) {
+                char c = s.charAt(i);
+                if (c >= '0' && c <= '9') {
+                    arr[count] = c;
+                    count++;
+                } else if (c >= 'A' && c <= 'Z') {
+                    arr[count] = MASK[c - 'A'];
+                    count++;
+                }
+
+                if (count == 3) {
+                    arr[count] = '-';
+                    count++;
+                }
+            }
+            rst.add(new String(arr));
+        }
+        return rst;
     }
 }

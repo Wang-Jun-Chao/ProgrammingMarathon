c889e1e3f2aa5cc17f23e0d24a4ef365815017d6
王俊超 <shining-glory@qq.com>
2016-05-23 08:17:57 +0800

    commit

---
+++ 083-目标柏林/src/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Author: 王俊超
+ * Time: 2016-05-23 08:15
+ * CSDN: http://blog.csdn.net/derrantcm
+ * Github: https://github.com/Wang-Jun-Chao
+ * Declaration: All Rights Reserved !!!
+ */
+public class Main {
+}

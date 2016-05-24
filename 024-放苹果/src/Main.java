16f8b7037c518570fcb53c8402241369b0d6661f
王俊超 <shining-glory@qq.com>
2016-05-24 23:00:13 +0800

    commit

--- 024-放苹果/src/Main.java
+++ 024-放苹果/src/Main.java
@@ -15,7 +15,7 @@ public class Main {
             int m = scanner.nextInt();
             int n = scanner.nextInt();
 
-            System.out.println(placeApple3(m, n));
+            System.out.println(placeApple2(m, n));
         }
 
         scanner.close();
@@ -36,46 +36,52 @@ public class Main {
      * 第二条m会逐渐减少，因为n>m时，我们会return f(m,m)　所以终会到达出口m==0．
      */
 
-    /**
-     * 放苹果
-     *
-     * @param m 苹果个数
-     * @param n 盘子个数
-     * @return 共的放法数目
-     */
-    private static int placeApple3(int m, int n) {
-
-        int row = m + 1;
-        int col = n + 1;
-
-        int[][] dp = new int[row][col];
-
-        for (int i = 1; i < row; i++) {
-            dp[i] = new int[n + 1];
-        }
-
-        for (int i = 1; i < row; i++) {
-            dp[i][0] = 1;
-        }
-
-        for (int i = 1; i < col; i++) {
-            dp[1][i] = 1;
-        }
-
-
-        for (int i = 1; i < row; i++) {
-            for (int j = 1; j < col; j++) {
-                if (j > i) {
-                    dp[i][j] = dp[i][i];
-                } else {
-                    dp[i][j] = dp[i][j - 1] + dp[i - j][j];
-                }
-            }
-        }
-
-
-        return dp[m - 1][n - 1];
-    }
+//    /**
+//     * 放苹果
+//     * 变形：求将一个整数m划分成n个数有多少种情况
+//     * dp[m][n] = dp[m-n][n] + dp[m-1][n-1]; 对于变形后的问题，存在两种情况：
+//     * 1. n 份中不包含 1 的分法，为保证每份都 >= 2，可以先拿出 n 个 1 分到每一份，
+//     *      然后再把剩下的 m- n 分成 n 份即可，分法有: dp[m-n][n]
+//     * 2. n 份中至少有一份为 1 的分法，可以先那出一个 1 作为单独的1份，剩下的 m- 1 再分成 n- 1 份即可，
+//     *      分法有：dp[m-1][n-1]
+//     *
+//     * @param m 苹果个数
+//     * @param n 盘子个数
+//     * @return 共的放法数目
+//     */
+//    private static int placeApple3(int m, int n) {
+//
+//        int row = m + 1;
+//        int col = n + 1;
+//
+//        int[][] dp = new int[row][col];
+//
+//        for (int i = 1; i < row; i++) {
+//            dp[i] = new int[n + 1];
+//        }
+//
+//        for (int i = 1; i < row; i++) {
+//            dp[i][0] = 1;
+//        }
+//
+//        for (int i = 1; i < col; i++) {
+//            dp[1][i] = 1;
+//        }
+//
+//
+//        for (int i = 1; i < row; i++) {
+//            for (int j = 1; j < col; j++) {
+//                if (j > i) {
+//                    dp[i][j] = dp[i][i];
+//                } else {
+//                    dp[i][j] = dp[i][j - 1] + dp[i - j][j];
+//                }
+//            }
+//        }
+//
+//
+//        return dp[m - 1][n - 1];
+//    }
 
 
     /**

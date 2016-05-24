ab110c73300b20ddf5016f48bddb1f55b4c0758a
王俊超 <shining-glory@qq.com>
2016-05-24 20:29:20 +0800

    commit

---
+++ 024-放苹果/src/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Author: 王俊超
+ * Time: 2016-05-24 20:28
+ * CSDN: http://blog.csdn.net/derrantcm
+ * Github: https://github.com/Wang-Jun-Chao
+ * Declaration: All Rights Reserved !!!
+ */
+public class Main {
+}

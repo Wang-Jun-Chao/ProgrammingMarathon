2cb297bd3977e41770b786e666475cf9e494a6e6
王俊超 <shining-glory@qq.com>
2016-05-25 10:22:04 +0800

    commit

--- 028-最长上升子序列/src/Main.java
+++ 028-最长上升子序列/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Author: 王俊超
  * Time: 2016-05-14 06:53
@@ -6,4 +8,128 @@
  * Declaration: All Rights Reserved !!!
  */
 public class Main {
+    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(Main.class.getClassLoader().getResourceAsStream("data.txt"));
+        while (scanner.hasNext()) {
+            int n = scanner.nextInt();
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = scanner.nextInt();
+            }
+
+            System.out.println(lis2(arr));
+        }
+
+        scanner.close();
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////
+    // O(n*log(n))算法实现
+    ////////////////////////////////////////////////////////////////////////////////////////////
+
+    /**
+     * 求最长公共子序列长度
+     *
+     * @param arr 数组
+     * @return 最长公共子序列长度
+     */
+    private static int lis2(int[] arr) {
+        int[] len = new int[arr.length];
+        int[] d = new int[arr.length];
+
+        d[0] = -1;
+        d[1] = arr[0];
+        len[0] = 1;
+
+        for (int i = 1, j; i < arr.length; i++) {
+            j = find(d, 0, arr.length - 1, arr[i]);
+            d[j] = arr[i];
+            len[i] = j;
+        }
+
+        int max = 1;
+        for (int i = 0; i < arr.length; i++) {
+            if (len[i] > max) {
+                max = len[i];
+            }
+        }
+
+        return max;
+    }
+
+    private static int find(int[] arr, int left, int right, int n) {
+        int mid;
+
+        while (left <= right) {
+            mid = left + (right - left) / 2;
+            if (n > arr[mid]) {
+                left = mid + 1;
+            } else if (n < arr[mid]) {
+                right = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+
+        return left;
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////
+    // O(n^2)算法实现
+    ////////////////////////////////////////////////////////////////////////////////////////////
+
+    /**
+     * 求最长公共子序列长度
+     *
+     * @param arr 数组
+     * @return 最长公共子序列长度
+     */
+    private static int lis(int[] arr) {
+        // len[i]表示包含arr[i]的序列的长度
+        int[] len = new int[arr.length];
+        // next[i]表示第i个下标的下一个数的下标是next[i]，值为-1表示结束
+        int[] next = new int[arr.length];
+
+
+        // 初始初化数据
+        len[len.length - 1] = 1;
+        for (int i = 0; i < arr.length; i++) {
+            next[i] = -1;
+            len[i] = 1;
+        }
+
+        for (int i = arr.length - 2; i >= 0; i--) {
+
+            // 找最大值和下标
+            for (int j = arr.length - 1; j > i; j--) {
+                if (arr[j] > arr[i] && len[j] >= len[i]) {
+                    // 记录最大值和前一个元素
+                    len[i] = len[j] + 1;
+                    next[i] = j;
+                }
+            }
+        }
+
+        // 找最大值和初始下标
+        int max = 0;
+        int j = -1;
+        for (int i = 0; i < len.length; i++) {
+            if (max < len[i]) {
+                max = len[i];
+                j = i;
+            }
+        }
+
+//        System.out.println(Arrays.toString(next));
+//        System.out.println(Arrays.toString(len));
+//        // 输出上升子序列
+//        while (j != -1) {
+//            System.out.print(arr[j] + " ");
+//            j = next[j];
+//        }
+//        System.out.println();
+
+        return max;
+    }
 }

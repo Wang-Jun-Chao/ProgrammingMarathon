8e7a5ab9158ceead9d9715c121112329b95c8cca
王俊超 <shining-glory@qq.com>
2016-05-27 06:24:59 +0800

    commit

--- 066-五子棋/src/Main.java
+++ 066-五子棋/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Author: 王俊超
  * Time: 2016-05-26 19:27
@@ -6,4 +8,75 @@
  * Declaration: All Rights Reserved !!!
  */
 public class Main {
+
+    // 四个方向两个一组，分别代表，右，右下，下，左下
+    private final static int[] D = {0, 1, 1, 1, 1, 0, 1, -1};
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+//        Scanner scanner = new Scanner(Main.class.getClassLoader().getResourceAsStream("data2.txt"));
+        int n = 20;
+        while (scanner.hasNext()) {
+            char[][] board = new char[n][n];
+            for (int i = 0; i < n; i++) {
+                board[i] = scanner.next().toCharArray();
+            }
+
+            if (checkBoard(board)) {
+                System.out.println("Yes");
+            } else {
+                System.out.println("No");
+            }
+        }
+
+        scanner.close();
+    }
+
+    private static boolean checkBoard(char[][] board) {
+        int len = board.length;
+        for (int i = 0; i < len; i++) {
+            for (int j = 0; j < len; j++) {
+                if (board[i][j] != '.' && checkBoard(board, i, j)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private static boolean checkBoard(char[][] board, int i, int j) {
+
+        int len = board.length;
+        char ch = board[i][j];
+
+        int endX;
+        int endY;
+
+        // 在四个方向上进行查找
+        for (int d = 0; d < D.length; d += 2) {
+            // 在(i, j)的基础上只要走四步就可以了
+            endX = i + D[d] * 4;
+            endY = j + D[d + 1] * 4;
+
+            // 边界条件要合法
+            if (endX > -1 && endX < len && endY > -1 && endY < len) {
+                int step = 0;
+                // 只要比较四次就可以了
+                for (int v = i + D[d], w = j + D[d + 1]; step < 4; v += D[d], w += D[d + 1], step++) {
+                    // 有不满足条件的就跳出内层循环
+                    if (board[v][w] != ch) {
+                        break;
+                    }
+                }
+
+                // 说明找找到了
+                if (step >= 4) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 }
+
--- 070-求面积/src/Main.java
+++ 070-求面积/src/Main.java
@@ -1,3 +1,7 @@
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
 /**
  * Author: 王俊超
  * Time: 2016-05-18 12:19
@@ -6,4 +10,108 @@
  * Declaration: All Rights Reserved !!!
  */
 public class Main {
+
+    // 右，下，左，上四个方向
+    private final static int[] D = {0, 1, 1, 0, 0, -1, -1, 0};
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+//        Scanner scanner = new Scanner(Main.class.getClassLoader().getResourceAsStream("data.txt"));
+        while (scanner.hasNext()) {
+            int n = scanner.nextInt();
+            int m = scanner.nextInt();
+            char[][] map = new char[n][n];
+            for (int i = 0; i < n; i++) {
+                map[i] = scanner.next().toCharArray();
+            }
+
+            System.out.println(square(map));
+        }
+
+        scanner.close();
+    }
+
+    private static int square(char[][] map) {
+
+//        for (char[] chs : map) {
+//            System.out.println(Arrays.toString(chs));
+//        }
+
+        int row = map.length;
+        int col = map[0].length;
+
+        // 将外层的0标记成1
+        for (int i = 0; i < row; i++) {
+            // 第一列
+            if (map[i][0] == '0') {
+                bfs(i, 0, map);
+            }
+            // 最后一列
+            if (map[i][col - 1] == '0') {
+                bfs(i, col - 1, map);
+            }
+        }
+
+        for (int i = 0; i < col; i++) {
+            // 第一行
+            if (map[0][i] == '0') {
+                bfs(0, i, map);
+            }
+            // 最后一行
+            if (map[row - 1][i] == '0') {
+                bfs(row - 1, i, map);
+            }
+        }
+
+        // 统计一的个数
+        int count = 0;
+        for (char[] line : map) {
+            for (int j = 0; j < col; j++) {
+                count += line[j] - '0';
+            }
+        }
+
+
+//        System.out.println();
+//        for (char[] chs : map) {
+//            System.out.println(Arrays.toString(chs));
+//        }
+
+        return row * col - count;
+    }
+
+    /**
+     * 从(x, y)处开始广度优先遍历，(x, y)是1之外的0
+     *
+     * @param x   行
+     * @param y   列
+     * @param map 地图
+     */
+    private static void bfs(int x, int y, char[][] map) {
+        List<Integer> list = new LinkedList<>();
+
+        list.add(x);
+        list.add(y);
+
+        int len = map.length;
+
+        map[x][y] = '1';
+
+        while (!list.isEmpty()) {
+            x = list.remove(0);
+            y = list.remove(0);
+
+            for (int i = 0; i < D.length; i += 2) {
+                int v = x + D[i];
+                int w = y + D[i + 1];
+
+                if (v > -1 && v < len && w > -1 && w < len && map[v][w] == '0') {
+                    map[v][w] = '1';
+                    list.add(v);
+                    list.add(w);
+                }
+            }
+        }
+    }
+
 }
